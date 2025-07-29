f957272d53ea56ea44e67ff9f95aeaa124f59eb0
PLACEMENT VMKVEC <placementvmkvec@gmail.com>
2025-07-29 14:49:45 +0000

    // For Firebase JS SDK v7.20.0 and later, measurementId is optional cons

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAncQG7Z0Y7XL9TTVNn8Cfji4NOu0Wqrwk",
-  authDomain: "vtechbridge.firebaseapp.com",
-  projectId: "vtechbridge",
-  storageBucket: "vtechbridge.firebasestorage.app",
-  messagingSenderId: "281899053981",
-  appId: "1:281899053981:web:b6c9abd8704b5b370a3574",
-  measurementId: "G-H5BGJCLZYD"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
